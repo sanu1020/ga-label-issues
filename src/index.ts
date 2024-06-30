fe705b20e2a1e5c32ff489f15add02fcde35d59c
sanu1020 <sanuuthaya02@gmail.com>
2024-06-30 17:17:32 +0530

    local added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import { getInput } from "@actions/core"
+import { getInput } from "@actions/core";
+import fetch from "node-fetch";
 
+async function run() {
+    const context = getInput("gh-context");
+    console.log("GITHUB_CONTEXT", context);
 
-export async function run() {
-  
-    const context = getInput("gh-context")
-    
-    console.log("GITHUB_CONTEXT", context)
+    const url = "http://localhost:3000/chartmanagement/context";
+    const requestBody = JSON.stringify({ context });
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: requestBody,
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const responseBody = await response.json();
+        console.log("Response from server:", responseBody);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
-
 
 run();
