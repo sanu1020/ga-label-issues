05d1de648a6e2586e740e431210352c097f903b0
sanu1020 <sanuuthaya02@gmail.com>
2024-06-30 12:39:42 +0530

    hey

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-import { getInput, setFailed } from "@actions/core"
-import { context, getOctokit } from "@actions/github";
+import { getInput } from "@actions/core"
+
 
 export async function run() {
-    const token = getInput("gh-token")
-    const label = getInput("label")
-
-    const octokit = getOctokit(token)
-    const pullRequest = context.payload.pull_request;
-
+  
+    const context = getInput("gh-context")
     
-    try{
-        if (!pullRequest)
-            throw new Error("This action can only be run on Pull Requests")
-        
-        await octokit.rest.issues.addLabels({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: pullRequest.number,
-            labels: [label],
-        })    
-    } catch (error) {
-        setFailed((error as Error)?.message ?? "Unknown error")
-    }
+    console.log("GITHUB_CONTEXT", context)
 }
 
-if (!process.env.JEST_WORKER_ID){
-    run();
-}
+
+run();
